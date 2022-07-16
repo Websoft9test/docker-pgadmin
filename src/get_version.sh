fe07ea2dfb24c38500c013f7fcc73f8b76fda114
qiaofeng1227 <76487013@qq.com>
2022-07-16 09:55:12 +0800

    Update get_version.sh

--- src/get_version.sh
+++ src/get_version.sh
@@ -1,1 +1,1 @@
-sudo echo Odoo Version:$(docker exec -i pgadmin sh -c 'cat docs/release_notes.html  |grep -i version |tail -1 |cut -d= -f4') 1>> /data/logs/install_version.txt
+sudo echo Pgadmin Version:$(docker exec -i pgadmin sh -c 'cat docs/release_notes.html  |grep -i version |tail -1 |cut -d= -f4') 1>> /data/logs/install_version.txt
